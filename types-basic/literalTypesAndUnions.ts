ef2fc68fd88e3e225cdc582769ce38e5e039d2b9
rebecaaras <rebecaaraujo2013@gmail.com>
2024-11-12 16:38:34 -0300

    new: Add fetchUserDetails with return type

--- types-basic/literalTypesAndUnions.ts
+++ types-basic/literalTypesAndUnions.ts
@@ -3,4 +3,24 @@ let myName1 = "Bob"
 const myName2 = "Bob"
 
 type UserRole = "guest" | "member" | "admin"
+type User = {
+    username: string
+    role: UserRole
+}
+
 let userRole: UserRole = "guest"
+
+//introducing function return types
+const users: User[] = [
+    {username: "john_doe", role: "member"},
+    {username: "jane_doe", role: "admin"},
+    {username: "jake_doe", role: "guest"}
+];
+
+function fetchUserDetails(username: string): User {
+    const user = users.find(user => user.username === username)
+    if (!user) {
+        throw new Error (`User with username ${username} not found.`)
+    }
+    return user
+}

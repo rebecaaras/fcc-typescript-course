d66ac3ea7c48927177247d63aee2c0d2c3776b55
rebecaaras <rebecaaraujo2013@gmail.com>
2024-11-13 19:40:08 -0300

    new: Introduce utility types Omit and Partial

--- types-basic/literalTypesAndUnions.ts
+++ types-basic/literalTypesAndUnions.ts
@@ -4,17 +4,20 @@ const myName2 = "Bob"
 
 type UserRole = "guest" | "member" | "admin"
 type User = {
+    id: number
     username: string
     role: UserRole
 }
 
-let userRole: UserRole = "guest"
+type UpdatedUser = Partial<User>
 
+let userRole: UserRole = "guest"
+let nextUserId = 1
 //introducing function return types
 const users: User[] = [
-    {username: "john_doe", role: "member"},
-    {username: "jane_doe", role: "admin"},
-    {username: "jake_doe", role: "guest"}
+    {id: nextUserId++, username: "john_doe", role: "member"},
+    {id: nextUserId++, username: "jane_doe", role: "admin"},
+    {id: nextUserId++, username: "jake_doe", role: "guest"}
 ];
 
 function fetchUserDetails(username: string): User {
@@ -24,3 +27,33 @@ function fetchUserDetails(username: string): User {
     }
     return user
 }
+
+//Partial utility type
+function updateUser(id: number, updates: UpdatedUser): void {
+    const foundUser = users.find(user => user.id === id)
+    if (!foundUser){
+        console.error(`User with id: ${id} not found.`)
+        return
+    }
+
+    Object.assign(foundUser, updates)
+}
+
+updateUser(1, { username: "new_john_doe"});
+//updateUser(4, { role: "contributor"});
+console.log(users);
+
+//Omit type
+function addNewUser(newUser: Omit<User, "id">): User {
+    const user: User = {
+        id: nextUserId++,
+        ...newUser //spreads all the other properties of newUser here
+    }
+    users.push(user)
+    
+    return user
+}
+
+addNewUser({ username: "joe_schmoe", role: "member" })
+
+console.log(users)
